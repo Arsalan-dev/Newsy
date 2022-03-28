7ef5be085b14fbe5ca8bf0f3cc925d68ee963e0a
Syed Arsalan <arsalan11772@gmail.com>
2022-03-28 02:54:11 +0200

    Adding CSS and NavBar to Newsy

---
+++ src/components/NewsItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>This is newsItem component</div>
+    )
+  }
+}
+
+export default NewsItem
