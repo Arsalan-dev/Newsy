1cbb13ae672c12079214480dd0c4c58e80bc7898
Syed Arsalan <arsalan11772@gmail.com>
2022-04-01 03:57:52 +0200

    Adding Next Prev buttons

--- src/components/News.js
+++ src/components/News.js
@@ -3,45 +3,91 @@ import NewsItem from './NewsItem'
 
 export class News extends Component {
 
-    constructor(){
+    constructor() {
         super();
         this.state = {
-            articles : [],
-            loading : false
+            articles: [],
+            isLoaded: false,
+            page: 1
 
         }
     }
 
-    async componentDidMount(){
-        let url = "https://newsapi.org/v2/top-headlines?country=de&apiKey=467c2a62bb7d47fb8252b66063bbac7d";
+    async componentDidMount() {
+        let url = "https://newsapi.org/v2/top-headlines?country=de&apiKey=467c2a62bb7d47fb8252b66063bbac7d&page=1&pagesize=20";
         let data = await fetch(url);
         let parsedData = await data.json();
-        console.log(parsedData);
-        this.setState({articles: parsedData.articles});
+        //console.log(parsedData);
+        this.setState({
+            articles: parsedData.articles,
+            isLoaded: true,
+            totalResults: parsedData.totalResults
+        });
+    }
+
+    handleNextClick = async () => {
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+
+        }
+        else {
+            let url = `https://newsapi.org/v2/top-headlines?country=de&apiKey=467c2a62bb7d47fb8252b66063bbac7d&page=${this.state.page + 1}&pagesize=20`;
+            let data = await fetch(url);
+            let parsedData = await data.json();
+            //console.log(parsedData);
+            this.setState({
+                articles: parsedData.articles,
+                page: this.state.page + 1
+            });
+        }
+
+    }
+
+    handlePrevClick = async () => {
+
+        let url = `https://newsapi.org/v2/top-headlines?country=de&apiKey=467c2a62bb7d47fb8252b66063bbac7d&page=${this.state.page - 1}&pagesize=20`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        //console.log(parsedData);
+        this.setState({
+            articles: parsedData.articles,
+            page: this.state.page - 1
+        });
+
+
     }
 
     render() {
-        return (
+        const { isLoaded } = this.state;
+        if (!isLoaded) {
+            return <div>Loading...</div>
+        }
+        else {
+            return (
 
-            <div className='container my-3' >
-                <h2>Newsy - Top Headlines</h2>
-                <div className="row my-3">
-                    {this.state.articles.map((element)=>{
-                        return <div className="col-md-4 my-3" key={element.url}>
-                        <NewsItem title={element.title.slice(0,36)} description={element.description.slice(0,73)} 
-                        imgUrl={element.urlToImage}
-                        newsUrl = {element.url} /></div>
+                <div className='container my-3' >
+                    <h2>Newsy - Top Headlines</h2>
+                    <div className="row my-3">
+                        {this.state.articles.map((element) => {
+                            return <div className="col-md-4 my-3" key={element.url}>
+                                <NewsItem title={element.title.slice(0, 36)} description={element.description.slice(0, 73)}
+                                    imgUrl={element.urlToImage}
+                                    newsUrl={element.url} />
+                            </div>
 
-                    })}
-                    
+                        })}
+
+                    </div>
+                    <div className="container d-flex justify-content-between">
+                        <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+                        <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+                    </div>
                 </div>
 
-            </div>
 
 
 
-
-        )
+            )
+        }
     }
 }
 
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,11 +6,11 @@ export class NewsItem extends Component {
         return (
             <div>
                 <div className="card" style={{width: "18rem"}}>
-                    <img src={imgUrl} className="card-img-top" alt="..."/>
+                    <img src={!imgUrl?"https://cdn.prod.www.spiegel.de/images/131968b7-ed2a-4c3f-95eb-ba7af2979154_w1280_r1.77_fpx33.34_fpy50.jpg":imgUrl} className="card-img-top" alt="..."/>
                         <div className="card-body">
                             <h5 className="card-title">{title} ...</h5>
                             <p className="card-text">{description} ...</p>
-                            <a href={newsUrl} target='_blank' className="btn btn-sm btn-primary">Read More</a>
+                            <a href={newsUrl} target='_blank' rel="noreferrer" className="btn btn-sm btn-primary btn-dark">Read More</a>
                         </div>
                 </div>
             </div>
